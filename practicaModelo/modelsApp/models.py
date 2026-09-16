a8840acf8dabbdd3a801c87774702149fc2930c0
informatica3 <informatica3@inacap.cl>
2026-09-16 14:53:31 -0300

    Config models

--- practicaModelo/modelsApp/models.py
+++ practicaModelo/modelsApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class EstudianteModels(models.Model):
+    nombre=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    edad=models.IntegerField()
