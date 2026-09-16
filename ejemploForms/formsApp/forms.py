fb67c95bc41f85977a8733a0457e7d586412c480
informatica3 <informatica3@inacap.cl>
2026-09-16 16:13:42 -0300

    forms practice

--- ejemploForms/formsApp/forms.py
+++ ejemploForms/formsApp/forms.py
@@ -1,2 +1,6 @@
 from django import forms
 
+class RegistroUsuarioForms(forms.Form):
+    nombre=forms.CharField()
+    email=forms.CharField(widget=forms.EmailInput)
+    edad=forms.IntegerField()
--- ejemploForms/formsApp/views.py
+++ ejemploForms/formsApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .forms import RegistroUsuarioForms
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    formulario = RegistroUsuarioForms()
+    data = {
+        'formulario':formulario
+    }
+    return render(request, 'index.html', data)
